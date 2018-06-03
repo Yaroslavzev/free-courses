0a8a48524ea9e0b54e2f9fd80bfd86029616c9c9
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-06-03 19:44:45 +0300

    task1
    v.2
    изменил ввывод

--- task1.rb
+++ task1.rb
@@ -1,10 +1,9 @@
 
 def loe(phrase)
   if phrase.downcase.scan(/\w/).eql?(phrase.downcase.scan(/\w/).reverse)
-    return 'YES'
+    puts 'YES'
   else
-    return 'NO'
+    puts 'NO'
   end
 end
-
-puts loe(ARGV[0].to_s)
+loe(ARGV[0].to_s)
