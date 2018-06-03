78d87a4f8a1520c882214da58ea87e1f57aa51a6
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-06-03 12:52:44 +0300

    Код для заданий
    Первая версия ответов на задания

---
+++ task2.rb
@@ -0,0 +1,16 @@
+aa = "января февраля марта апреля мая июня июля августа сентября октября ноября декабря".scan(/[а-яА-ЯЁё]{3,}/)
+
+year = ARGV[2].to_s
+month = aa.index(ARGV[1].to_s) + 1
+day = ARGV[0].to_s
+
+def days22 (x,y,z)
+  a = Time.local(x,y,z,0,0,0)
+  x1 = x.to_i + 1
+  b = Time.local(x1,1,1,0,0,0)
+  c = (b - a)
+  d = (c/86400).to_i
+  return d - 1
+end
+
+puts days22(year,month,day)
