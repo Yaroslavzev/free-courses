0c193816b5bce1a0cc446d770bd0c96bfa392858
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-06-05 14:19:46 +0300

    task2
    task2 v2

--- task2.rb
+++ task2.rb
@@ -1,17 +1,20 @@
+class User
+  def initialize(year,month,day)
+    aa = "января февраля марта апреля мая июня июля августа сентября октября ноября декабря".scan(/[а-яА-ЯЁё]{3,}/)
+    @year = year.to_s
+    @month = (aa.index(month) + 1).to_s
+    @day = day.to_s
+  end
 
-aa = "января февраля марта апреля мая июня июля августа сентября октября ноября декабря".scan(/[а-яА-ЯЁё]{3,}/)
-
-year = ARGV[2].to_s
-month = aa.index(ARGV[1].to_s) + 1
-day = ARGV[0].to_s
-
-def days22 (x,y,z)
-  a = Time.local(x,y,z,0,0,0)
-  x1 = x.to_i + 1
-  b = Time.local(x1,1,1,0,0,0)
-  c = (b - a)
-  d = (c/86400).to_i
-  return d
+  def days
+    a = Time.local(@year,@month,@day,0,0,0)
+    year1 = @year.to_i + 1
+    b = Time.local(year1,1,1,0,0,0)
+    c = (b - a)
+    d = (c/86400).to_i
+    return d
+  end
 end
 
-puts days22(year,month,day)
+newdata = User.new(ARGV[2].to_s,ARGV[1].to_s,ARGV[0].to_s)
+puts newdata.days
